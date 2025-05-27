afb6196b22d408644bd4b3934790f924eba8736d
leopham <hongthaipro@gmail.com>
2025-05-27 14:53:17 +0700

    Update Anchor.toml and lib.rs to change program ID and implement deposit/withdraw functions; modify tests for new functionality

--- tests/blueshift_anchor_vault.ts
+++ tests/blueshift_anchor_vault.ts
@@ -6,11 +6,18 @@ describe("blueshift_anchor_vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.blueshiftAnchorVault as Program<BlueshiftAnchorVault>;
-
-  it("Is initialized!", async () => {
+  const program = anchor.workspace
+    .blueshiftAnchorVault as Program<BlueshiftAnchorVault>;
+  const amount = new anchor.BN(1_000_000); // 1 million lamports
+  it("can deposit", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.deposit(amount).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("can withdraw", async () => {
+    // Add your test here.
+    const tx = await program.methods.withdraw().rpc();
     console.log("Your transaction signature", tx);
   });
 });
